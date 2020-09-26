4ff26b86484d59266ab7d4e5560255e3f972bf39
Peter <Posoo617@gmail.com>
2020-09-26 13:30:01 +0300

    UseReducer

--- react-render-demo/src/App.js
+++ react-render-demo/src/App.js
@@ -1,12 +1,14 @@
 import React, { useReducer } from "react"
 import "./App.css"
 import { UseState } from "./components/UseState/UseState"
+import { UseReducer } from "./components/UseReducer/UseReducer"
 
 const App = () => {
 	return (
 		<div className="App">
 			<h1>React Render</h1>
 			<UseState />
+			<UseReducer />
 		</div>
 	)
 }
