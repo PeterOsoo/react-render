5c02643b86dad491239bd1d838e767adc4442576
Peter <Posoo617@gmail.com>
2020-09-26 13:28:18 +0300

    useState

--- react-render-demo/src/App.js
+++ react-render-demo/src/App.js
@@ -1,9 +1,14 @@
-import React from "react"
+import React, { useReducer } from "react"
 import "./App.css"
+import { UseState } from "./components/UseState/UseState"
 
 const App = () => {
-	console.log("rendering app")
-	return <h1>React Render</h1>
+	return (
+		<div className="App">
+			<h1>React Render</h1>
+			<UseState />
+		</div>
+	)
 }
 
 export default App
